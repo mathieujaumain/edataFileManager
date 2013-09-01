42095aa3446c017f11fb0e9f1e7210610f121d9d
Johan Karlsson <the.mad.moose@gmail.com>
2013-09-02 00:06:56 +0200

    Added file prompt at startup
    Allows the user to choose what file to open.

--- EdataFileManager/ViewModel/ManagerMainViewModel.cs
+++ EdataFileManager/ViewModel/ManagerMainViewModel.cs
@@ -16,15 +16,21 @@ namespace EdataFileManager.ViewModel
         public ICommand ExportNdfCommand { get; set; }
         public ICommand ExportTextureCommand { get; set; }
 
+        public string current_file {get; set; }
+
+        public string export_path { get; set; }
+
+
         protected void ExportNdfExecute(object obj)
         {
+            
             var file = obj as NdfFile;
 
             var content = NdfManager.GetNdfContent(file);
 
             var f = new FileInfo(file.Path);
 
-            using (var fs = new FileStream(@"C:\" + f.Name, FileMode.OpenOrCreate))
+            using (var fs = new FileStream(export_path + f.Name, FileMode.OpenOrCreate))
             {
                 fs.Write(content.Body, 0, content.Body.Length);
                 fs.Flush();
@@ -39,7 +45,7 @@ namespace EdataFileManager.ViewModel
 
             var f = new FileInfo(file.Path);
 
-            using (var fs = new FileStream(@"C:\" + f.Name, FileMode.OpenOrCreate))
+            using (var fs = new FileStream(export_path + f.Name, FileMode.OpenOrCreate))
             {
                 fs.Write(buffer, 0, buffer.Length);
                 fs.Flush();
@@ -53,9 +59,25 @@ namespace EdataFileManager.ViewModel
 
 
             //@"E:\Programme\Steam\SteamApps\common\Wargame Airland Battle\Data\wargame\PC\2100001470\NDF_Win.dat"
+            export_path = @"D:\SCIENCE\";
 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = @"D:\Steam\SteamApps\common\Wargame Airland Battle\Data\wargame\PC\2060001225\ZZ_3.dat";
+            dlg.DefaultExt = ".dat";
+            dlg.Filter = "Edat (.dat)|*.dat";
 
-            NdfManager = new NdfBinManager(@"E:\Programme\Steam\SteamApps\common\Wargame Airland Battle\Data\wargame\PC\2060001225\ZZ_3.dat");
+            bool? result = dlg.ShowDialog();
+
+            if (result == true)
+            {
+                current_file = dlg.FileName;
+            }
+            else
+            {
+                current_file = @"D:\Steam\SteamApps\common\Wargame Airland Battle\Data\wargame\PC\2060001225\ZZ_3.dat";
+            }
+
+            NdfManager = new NdfBinManager(current_file);
 
             NdfManager.ParseEdataFile();
             Files = NdfManager.Files;
